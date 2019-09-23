2fda30cf0b0880fa7fff6cf3ec88297c19c90ea9
Mitar <mitar.git@tnode.com>
2019-09-22 23:00:12 -0700

    Version bump.

--- package.js
+++ package.js
@@ -1,25 +1,25 @@
 Package.describe({
   name: 'peerlibrary:blaze-layout-component',
   summary: "A simple Blaze Component for use with Flow Router's layout manager",
-  version: '0.2.1',
+  version: '0.3.0',
   git: 'https://github.com/peerlibrary/meteor-blaze-layout-component.git'
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom('METEOR@1.4.1');
+  api.versionsFrom('METEOR@1.8.1');
 
   // Core dependencies.
   api.use([
-    'coffeescript',
+    'coffeescript@2.4.1',
     'underscore'
   ]);
 
   // 3rd party dependencies.
   api.use([
-    'peerlibrary:blaze-components@0.20.0',
+    'peerlibrary:blaze-components@0.23.0',
     'kadira:blaze-layout@2.3.0',
-    'peerlibrary:computed-field@0.6.0',
-    'peerlibrary:assert@0.2.5'
+    'peerlibrary:computed-field@0.10.0',
+    'peerlibrary:assert@0.3.0'
   ]);
 
   api.export('BlazeLayoutComponent');
